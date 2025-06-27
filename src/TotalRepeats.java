e7e3a7716a08cb0959b7562a14828f45ff2a1ead
Ruslan Kalendar <78969155+rkalendar@users.noreply.github.com>
2025-06-27 11:22:56 +0500

    Update TotalRepeats.java

--- src/TotalRepeats.java
+++ src/TotalRepeats.java
@@ -184,11 +184,11 @@ public class TotalRepeats {
             System.out.println("java -jar \\TotalRepeats\\dist\\TotalRepeats.jar <inputfile> kmer=18 sln=100 mask=false seqshow=true flanks=100\n");
             System.out.println("java -jar \\TotalRepeats\\dist\\TotalRepeats.jar E:\\Genomes\\T2T-CHM13v2.0\\ -ref=C:\\TotalRepeats\\test\\humsub.ref\n");
             System.out.println("Large genome usage (you will have to show the program to use more RAM, for example as listed here, up to 64 Gb memory: -Xms16g -Xmx64g):");
-            System.out.println("java -jar -Xms16g -Xmx64g C:\\TotalRepeats\\dist\\TotalRepeats.jar E:\\Genomes\\T2T-CHM13v2.0\\ nsize=1 kmer=21 sln=90 \n");
-            System.out.println("For chromosomes larger than 500 Mb you will need to use more memory, 128 Gb:");
-            System.out.println("java -jar -Xms32g -Xmx128g C:\\TotalRepeats\\dist\\TotalRepeats.jar E:\\Genomes\\Cycas_panzhihuaensis\\ nsize=1 kmer=21 sln=90\n");
+            System.out.println("java -jar -Xms16g -Xmx64g C:\\TotalRepeats\\dist\\TotalRepeats.jar E:\\Genomes\\T2T-CHM13v2.0\\\n");
+            System.out.println("For chromosomes larger than 500 Mb you will need to use more memory, 128-256 Gb (-Xmx128g or -Xmx256g):");
+            System.out.println("java -jar -Xms32g -Xmx128g C:\\TotalRepeats\\dist\\TotalRepeats.jar E:\\Genomes\\Cycas_panzhihuaensis\\\n");
             System.out.println("Analysing all files in the directory:");
-            System.out.println("java -jar \\TotalRepeats\\dist\\TotalRepeats.jar \\TotalRepeats\\test\\\n");
+            System.out.println("java -jar -Xms16g -Xmx64g C:\\TotalRepeats\\dist\\TotalRepeats.jar E:\\Genomes\\T2T-CHM13v2.0\\\n");
         }
     }
 
